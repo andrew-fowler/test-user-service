85105c9a2692834072958ed37ccf07998be26188
andrew-fowler <4ndrew.fowler@gmail.com>
2017-03-10 19:47:03 +0000

    Various html tweaks

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,23 @@
-from flask_wtf import Form
+from flask_wtf import Form, validators
 from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import validators
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length
 
 
 class EmailForm(Form):
-    email = StringField('email', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired(), validators.Email()])
 
 
 class BulkCreateForm(Form):
-    prefix = StringField('prefix', validators=[DataRequired()])
-    domain = StringField('domain', validators=[DataRequired()])
-    count = StringField('count', validators=[DataRequired()])
+    prefix = StringField('prefix',
+                         validators=[DataRequired(), Length(min=3, max=25)],
+                         default='test')
+
+    domain = StringField('domain',
+                         validators=[DataRequired(), Length(min=3, max=25)],
+                         default='test.com')
+
+    count = StringField('count',
+                        validators=[DataRequired(), Length(min=1, max=2)],
+                        default='5')
